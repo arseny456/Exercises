b202e32015a47f5e9c7e48ca702733a5956a7bcf
your name <you@example.com>
2021-10-10 11:35:54 +0300

    first commit1

--- Exr_3.py
+++ Exr_3.py
@@ -1,2 +1,23 @@
 class Counter:
-    
+    value = 0
+
+    def start_from(self, value=0):
+        self.value = value
+
+    def increment(self):
+        self.value += 1
+
+    def reset(self):
+        self.value = 0
+
+    def display(self):
+        print('Текущее значение счтчика' + str(self.value))
+
+
+cl = Counter()
+cl.start_from()
+cl.increment()
+cl.increment()
+cl.display()
+cl.reset()
+cl.display()
